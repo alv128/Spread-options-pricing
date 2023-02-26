cb5f58f08668f9f5be0ee593ace3cff964da165f
Alvin <alvinpyngottu@gmail.com>
2023-02-26 19:41:25 +0100

    reformulating for alternative option price formulation

--- spread_option.py
+++ spread_option.py
@@ -25,9 +25,10 @@ import tensorflow.compat.v2 as tf
 from tf_quant_finance.black_scholes import vanilla_prices
 
 #spread option price: P_0(S_1, S_2, t) = S_1*N(d_1) - (S_2 + K*e^(-rt))*N(d_2)
-#S = D*F, F = S/D forward price, D = e^(-rt)
 #regular option price:       P_0(S, t) = S*N(d_1) - K*e^(-rt)*N(d_2)
+#S = D*F, F = S/D forward price, D = e^(-rt)
 
+#Kirk's approximation using WKB method for the two-dimensional Black-Scholes models for spread-options
 def spread_option_price(volatilities1,
                         volatilities2,
                         correlations,
@@ -53,6 +54,7 @@ def spread_option_price(volatilities1,
     volatilities2 = tf.convert_to_tensor(
         volatilities2, dtype=dtype, name='volatilities2')
     expiries = tf.convert_to_tensor(expiries, dtype=dtype, name='expiries')
+    correlations = tf.convert_to_tensor(correlations, dtype=dtype, name='correlations')
 
     if (spots1 is None) == (forwards1 is None):
         if (spots2 is None) == (forwards2 is None):
@@ -100,87 +102,31 @@ def spread_option_price(volatilities1,
         forwards1 = spots1 * tf.exp((discount_rates - dividend_rates) * expiries)
         forwards2 = spots2 * tf.exp((discount_rates - dividend_rates) * expiries)
         
-    sqrt_var1 = volatilities1 * tf.math.sqrt(expiries)#sigma_s * sqrt(t)
-    sqrt_var2 = volatilities2 * tf.math.sqrt(expiries)
-    if not is_normal_volatility:  # lognormal model
-        d1 = tf.math.divide_no_nan(tf.math.log(forwards / strikes),#ln(S/K)
-                                 sqrt_var) + sqrt_var / 2
-        d2 = d1 - sqrt_var
-        undiscounted_calls = tf.where(sqrt_var > 0,
-                                        forwards * _ncdf(d1) - strikes * _ncdf(d2),
-                                        tf.math.maximum(forwards - strikes, 0.0))
-    else:  # normal model
-        d1 = tf.math.divide_no_nan((forwards - strikes), sqrt_var)
-        undiscounted_calls = tf.where(
-            sqrt_var > 0.0, (forwards - strikes) * _ncdf(d1) +
-            sqrt_var * tf.math.exp(-0.5 * d1**2) / np.sqrt(2 * np.pi),
-            tf.math.maximum(forwards - strikes, 0.0))
+    #sqrt_var1 = volatilities1 * tf.math.sqrt(expiries)#sigma_s * sqrt(t)
+    #sqrt_var2 = volatilities2 * tf.math.sqrt(expiries)
+
+    #sqrt_var_eff = sqrt_var2 * (forwards2 / (forwards2 + strikes)) #This is the formula
+    sqrt_var_eff = volatilities2 * tf.math.divide_no_nan(forwards2, (forwards2 + strikes))#is no nan needed?
+    #volalities are not squared
+    sqrt_var_ = tf.math.sqrt(tf.math.square(volatilities1) - 2 * correlations * sqrt_var_eff + tf.math.quare(sqrt_var_eff))
+    sqrt_var = sqrt_var_ * tf.math.sqrt(expiries)
+
+    #if not is_normal_volatility:  # lognormal model # Don't need to differentiate between modes 
+    d1 = tf.math.divide_no_nan(tf.math.log(forwards1 / (forwards2 + strikes)), sqrt_var) + sqrt_var / 2
+    d2 = d1 - sqrt_var
+    #d1 = tf.math.divide_no_nan(tf.math.log(forwards / strikes),#ln(S/K)
+    #                         sqrt_var) + sqrt_var / 2
+    #d2 = d1 - sqrt_var
+    undiscounted_calls = tf.where(sqrt_var > 0,
+                                    forwards1 * _ncdf(d1) - (forwards2 + strikes) * _ncdf(d2),
+                                    tf.math.maximum(forwards - strikes, 0.0))#TODO
+    if is_call_options is None:
+        return discount_factors * undiscounted_calls
+    
 
 
+def _ncdf(x):
+  return (tf.math.erf(x / _SQRT_2) + 1) / 2
 
 
-
-    #Kirk's approximation using WKB method for the two-dimensional Black-Scholes models for spread-options
-    with tf.name_scope(name or 'spread_option_price'):
-
-        strikes = tf.convert_to_tensor(strikes, dtype=dtype, name='strikes')
-        #strikes = tf.convert_to_tensor(strikes, dtype=dtype, name='strike_price')#2xn tensor
-        dtype = strikes.dtype
-        volatilities = tf.convert_to_tensor(volatilities, dtype=dtype, name='volatilities')#2xn tensor
-        expiries = tf.convert_to_tensor(expiries, dtype=dtype, name='expiries')
-        correlations = tf.convert_to_tensor(correlations, dtype=dtype, name='correlations')#1x1
-        
-        # dividend_rates = tf.convert_to_tensor(dividend_rates, dtype=dtype, name='dividend_rates')#1xn
-        # spots = tf.convert_to_tensor(spots, dtype=dtype, name='spots')#2xn tensor, for the two assets 
-
-        if discount_rates is not None:
-            discount_rates = tf.convert_to_tensor(discount_rates, dtype=dtype, name='discount_rates')
-            discount_factors = tf.exp(-discount_rates * expiries)
-        elif discount_factors is not None:
-            discount_factors = tf.convert_to_tensor(discount_factors, dtype=dtype, name='discount_factors')
-            discount_rates = -tf.math.log(discount_factors) / expiries
-        else:
-            discount_rates = tf.convert_to_tensor(0.0, dtype=dtype, name='discount_rates')
-            discount_factors = tf.convert_to_tensor(1.0, dtype=dtype, name='discount_factors')
-
-        if dividend_rates is None:
-            dividend_rates = tf.convert_to_tensor(
-                0.0, dtype=dtype, name='dividend_rates')
-
-        if forwards is not None:
-            forwards = tf.convert_to_tensor(forwards, dtype=dtype, name='forwards')
-            spots = forwards * tf.exp(-(discount_rates - dividend_rates) * expiries)
-        else:
-            spots = tf.convert_to_tensor(spots, dtype=dtype, name='spots')
-            forwards = spots * tf.exp((discount_rates - dividend_rates) * expiries)
-
-
-        #calculate sigma_eff, if constant for sigma in Kirk's approximation is provided
-        spot1 = spots[0][0]
-        spot2 = spots[1][0]
-        var2 = volatilities[1][0]
-        var1 = volatilities[0][0]
-        sigma_eff = var2 * (spot2/(spot2 + strikes[0][0]*tf.exp(-dividend_rates[0]*expiries[0])))
-
-        #if sigma_const==None:# use WKB approximation
-        sigma_ = tf.math.sqrt(var1 - 2*correlations[0]*tf.math.sqrt(var1)*tf.math.sqrt(sigma_eff) + sigma_eff)
-        #else:
-        #    sigma_ = tf.math.sqrt(var1 - 2*correlations[0]*tf.math.sqrt(var1)*tf.math.sqrt(sigma_const) + sigma_const)
-
-        d1 = tf.math.log(spot1) - tf.math.log(spot2 + strikes[0][0]*tf.exp(-dividend_rates[0]*expiries[0]))
-        d1 = d1/(tf.math.sqrt(sigma_*expiries[0])) + 0.5*tf.math.sqrt(sigma_*expiries[0])
-        d2 = d1 + tf.math.sqrt(sigma_*expiries[0])
-
-        #regular black scholes model for option price
-        #C=SN(d1) - Kexp(-rt)N(d2)
-        #use regular option pricing with above variables as input parameters
-
-        return vanilla_prices.option_price(
-        volatilities=volatilities,
-        strikes=strikes,
-        expiries=expiries,
-        #forwards=asian_forwards,
-        dividend_rates=dividend_rates,
-        #discount_factors=discount_factors,
-        is_call_options=is_call_options,
-        dtype=dtype)
+_SQRT_2 = np.sqrt(2.0, dtype=np.float64)
